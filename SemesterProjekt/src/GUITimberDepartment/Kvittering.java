027de7d04d37748e5a2108c94040b9db7bd475de
NielsBH <117630767+NielsBH@users.noreply.github.com>
2023-01-16 14:02:57 +0100

    kvittering gui
    gui

--- SemesterProjekt/src/GUITimberDepartment/Kvittering.java
+++ SemesterProjekt/src/GUITimberDepartment/Kvittering.java
@@ -15,18 +15,39 @@ import javax.swing.JTable;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JList;
+import java.awt.TextArea;
 
 public class Kvittering extends JDialog {
-	private JTable table_3;
-	private JTable table;
 	private JTable table_1;
-	private JTable table_4;
-	private JLabel lblProductname;
-	private JLabel lblAmount;
-	private JLabel lblPriceWithTax;
-	private JLabel lblTax;
+	private JTable table_2;
+	private JTable table_3;
+	private JLabel lblNewLabel_1;
+	private JLabel lblNewLabel_2;
 	private JLabel lblNewLabel_5;
-	private JLabel lblSlgernavn;
+	private JLabel lblNewLabel_3;
+	private JButton btnNewButton;
+	private JTable table;
+	private JPanel panel_1;
+	private JLabel lblNewLabel_6;
+	private JTable table_6;
+	private JTable table_4;
+	private JTable table_5;
+	private JLabel lblNewLabel_4;
+	private JLabel lblNewLabel_7;
+	private JTable table_7;
+	private JLabel lblNewLabel_8;
+	private JTextField textField;
+	private JLabel lblNewLabel_9;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_10;
+	private JTable table_8;
+	private JTable table_9;
+	private JTable table_10;
 
 	/**
 	 * Launch the application.
@@ -45,120 +66,254 @@ public class Kvittering extends JDialog {
 	 * Create the dialog.
 	 */
 	public Kvittering() {
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		{
-			JPanel buttonPane = new JPanel();
-			getContentPane().add(buttonPane);
-			GridBagLayout gbl_buttonPane = new GridBagLayout();
-			gbl_buttonPane.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 60, -14, -16, 63, 0, 0, 0, 0, 15, 73, 19, 57, 0};
-			gbl_buttonPane.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-			gbl_buttonPane.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-			gbl_buttonPane.rowWeights = new double[]{0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-			buttonPane.setLayout(gbl_buttonPane);
-			
-			JLabel lblNewLabel = new JLabel("Kvittering");
-			lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
-			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-			gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-			gbc_lblNewLabel.gridx = 6;
-			gbc_lblNewLabel.gridy = 0;
-			buttonPane.add(lblNewLabel, gbc_lblNewLabel);
-			
-			lblNewLabel_5 = new JLabel("Sælger:");
-			GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-			gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
-			gbc_lblNewLabel_5.gridx = 10;
-			gbc_lblNewLabel_5.gridy = 0;
-			buttonPane.add(lblNewLabel_5, gbc_lblNewLabel_5);
-			
-			lblSlgernavn = new JLabel("SÆLGERNAVN");
-			lblSlgernavn.setHorizontalAlignment(SwingConstants.CENTER);
-			GridBagConstraints gbc_lblSlgernavn = new GridBagConstraints();
-			gbc_lblSlgernavn.insets = new Insets(0, 0, 5, 5);
-			gbc_lblSlgernavn.gridx = 13;
-			gbc_lblSlgernavn.gridy = 0;
-			buttonPane.add(lblSlgernavn, gbc_lblSlgernavn);
-			
-			lblProductname = new JLabel("Produkt navn");
-			GridBagConstraints gbc_lblProductname = new GridBagConstraints();
-			gbc_lblProductname.anchor = GridBagConstraints.WEST;
-			gbc_lblProductname.insets = new Insets(0, 0, 5, 5);
-			gbc_lblProductname.gridx = 2;
-			gbc_lblProductname.gridy = 1;
-			buttonPane.add(lblProductname, gbc_lblProductname);
-			
-			lblAmount = new JLabel("Antal");
-			GridBagConstraints gbc_lblAmount = new GridBagConstraints();
-			gbc_lblAmount.insets = new Insets(0, 0, 5, 5);
-			gbc_lblAmount.gridx = 6;
-			gbc_lblAmount.gridy = 1;
-			buttonPane.add(lblAmount, gbc_lblAmount);
-			
-			lblPriceWithTax = new JLabel("Pris m. moms");
-			GridBagConstraints gbc_lblPriceWithTax = new GridBagConstraints();
-			gbc_lblPriceWithTax.gridwidth = 2;
-			gbc_lblPriceWithTax.insets = new Insets(0, 0, 5, 5);
-			gbc_lblPriceWithTax.gridx = 9;
-			gbc_lblPriceWithTax.gridy = 1;
-			buttonPane.add(lblPriceWithTax, gbc_lblPriceWithTax);
-			
-			lblTax = new JLabel("Moms");
-			GridBagConstraints gbc_lblTax = new GridBagConstraints();
-			gbc_lblTax.gridwidth = 2;
-			gbc_lblTax.insets = new Insets(0, 0, 5, 5);
-			gbc_lblTax.gridx = 13;
-			gbc_lblTax.gridy = 1;
-			buttonPane.add(lblTax, gbc_lblTax);
-			
-			table_3 = new JTable();
-			GridBagConstraints gbc_table_3 = new GridBagConstraints();
-			gbc_table_3.gridheight = 6;
-			gbc_table_3.gridwidth = 4;
-			gbc_table_3.insets = new Insets(0, 0, 5, 5);
-			gbc_table_3.fill = GridBagConstraints.BOTH;
-			gbc_table_3.gridx = 1;
-			gbc_table_3.gridy = 2;
-			buttonPane.add(table_3, gbc_table_3);
-			
-			table = new JTable();
-			GridBagConstraints gbc_table = new GridBagConstraints();
-			gbc_table.gridheight = 6;
-			gbc_table.gridwidth = 4;
-			gbc_table.insets = new Insets(0, 0, 5, 5);
-			gbc_table.fill = GridBagConstraints.BOTH;
-			gbc_table.gridx = 5;
-			gbc_table.gridy = 2;
-			buttonPane.add(table, gbc_table);
-			
-			table_1 = new JTable();
-			GridBagConstraints gbc_table_1 = new GridBagConstraints();
-			gbc_table_1.gridheight = 6;
-			gbc_table_1.gridwidth = 4;
-			gbc_table_1.insets = new Insets(0, 0, 5, 5);
-			gbc_table_1.fill = GridBagConstraints.BOTH;
-			gbc_table_1.gridx = 9;
-			gbc_table_1.gridy = 2;
-			buttonPane.add(table_1, gbc_table_1);
-			
-			table_4 = new JTable();
-			GridBagConstraints gbc_table_4 = new GridBagConstraints();
-			gbc_table_4.gridwidth = 2;
-			gbc_table_4.gridheight = 6;
-			gbc_table_4.insets = new Insets(0, 0, 5, 5);
-			gbc_table_4.fill = GridBagConstraints.BOTH;
-			gbc_table_4.gridx = 13;
-			gbc_table_4.gridy = 2;
-			buttonPane.add(table_4, gbc_table_4);
-			
-			JButton btnNewButton = new JButton("Færdig");
-			GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
-			gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
-			gbc_btnNewButton.gridwidth = 3;
-			gbc_btnNewButton.gridx = 13;
-			gbc_btnNewButton.gridy = 8;
-			buttonPane.add(btnNewButton, gbc_btnNewButton);
-		}
+		setBounds(100, 100, 684, 593);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 224, 162, 0, 0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{32, 0, 0, 26, 92, 0, 29, 36, 33, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		getContentPane().setLayout(gridBagLayout);
+		
+		lblNewLabel_5 = new JLabel("Kvittering");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 1;
+		gbc_lblNewLabel_5.gridy = 0;
+		getContentPane().add(lblNewLabel_5, gbc_lblNewLabel_5);
+		
+		lblNewLabel_9 = new JLabel("Dato");
+		GridBagConstraints gbc_lblNewLabel_9 = new GridBagConstraints();
+		gbc_lblNewLabel_9.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_9.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_9.gridx = 2;
+		gbc_lblNewLabel_9.gridy = 0;
+		getContentPane().add(lblNewLabel_9, gbc_lblNewLabel_9);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.gridwidth = 4;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 3;
+		gbc_textField_1.gridy = 0;
+		getContentPane().add(textField_1, gbc_textField_1);
+		
+		lblNewLabel_8 = new JLabel("Klokkeslæt");
+		lblNewLabel_8.setVerticalAlignment(SwingConstants.TOP);
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_8.gridx = 2;
+		gbc_lblNewLabel_8.gridy = 1;
+		getContentPane().add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.gridwidth = 4;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 3;
+		gbc_textField_2.gridy = 1;
+		getContentPane().add(textField_2, gbc_textField_2);
+		
+		lblNewLabel_10 = new JLabel("Sælger");
+		lblNewLabel_10.setVerticalAlignment(SwingConstants.TOP);
+		GridBagConstraints gbc_lblNewLabel_10 = new GridBagConstraints();
+		gbc_lblNewLabel_10.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_10.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_10.gridx = 2;
+		gbc_lblNewLabel_10.gridy = 2;
+		getContentPane().add(lblNewLabel_10, gbc_lblNewLabel_10);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.gridwidth = 4;
+		gbc_textField.insets = new Insets(0, 0, 5, 5);
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 3;
+		gbc_textField.gridy = 2;
+		getContentPane().add(textField, gbc_textField);
+		textField.setColumns(10);
+		
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.gridheight = 8;
+		gbc_panel.insets = new Insets(0, 0, 5, 5);
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 1;
+		gbc_panel.gridy = 1;
+		getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{11, 101, 0, 0, 62, 16, 0};
+		gbl_panel.rowHeights = new int[]{0, 173, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel lblNewLabel = new JLabel("Produktnavn");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 0;
+		panel.add(lblNewLabel, gbc_lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Antal");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.gridx = 2;
+		gbc_lblNewLabel_1.gridy = 0;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
+		
+		lblNewLabel_2 = new JLabel("Pris m moms");
+		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
+		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.gridx = 3;
+		gbc_lblNewLabel_2.gridy = 0;
+		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("Moms");
+		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
+		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_3.gridx = 4;
+		gbc_lblNewLabel_3.gridy = 0;
+		panel.add(lblNewLabel_3, gbc_lblNewLabel_3);
+		
+		table_1 = new JTable();
+		GridBagConstraints gbc_table_1 = new GridBagConstraints();
+		gbc_table_1.insets = new Insets(0, 0, 0, 5);
+		gbc_table_1.fill = GridBagConstraints.BOTH;
+		gbc_table_1.gridx = 1;
+		gbc_table_1.gridy = 1;
+		panel.add(table_1, gbc_table_1);
+		
+		table = new JTable();
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.insets = new Insets(0, 0, 0, 5);
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 2;
+		gbc_table.gridy = 1;
+		panel.add(table, gbc_table);
+		
+		table_2 = new JTable();
+		GridBagConstraints gbc_table_2 = new GridBagConstraints();
+		gbc_table_2.insets = new Insets(0, 0, 0, 5);
+		gbc_table_2.fill = GridBagConstraints.BOTH;
+		gbc_table_2.gridx = 3;
+		gbc_table_2.gridy = 1;
+		panel.add(table_2, gbc_table_2);
+		
+		table_3 = new JTable();
+		GridBagConstraints gbc_table_3 = new GridBagConstraints();
+		gbc_table_3.insets = new Insets(0, 0, 0, 5);
+		gbc_table_3.fill = GridBagConstraints.BOTH;
+		gbc_table_3.gridx = 4;
+		gbc_table_3.gridy = 1;
+		panel.add(table_3, gbc_table_3);
+		
+		panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridwidth = 5;
+		gbc_panel_1.gridheight = 2;
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 2;
+		gbc_panel_1.gridy = 7;
+		getContentPane().add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[]{16, 18, 9, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0};
+		gbl_panel_1.rowWeights = new double[]{1.0, 1.0, 1.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		lblNewLabel_6 = new JLabel("Rabat");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_6.gridx = 1;
+		gbc_lblNewLabel_6.gridy = 0;
+		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		table_4 = new JTable();
+		GridBagConstraints gbc_table_4 = new GridBagConstraints();
+		gbc_table_4.insets = new Insets(0, 0, 5, 5);
+		gbc_table_4.fill = GridBagConstraints.BOTH;
+		gbc_table_4.gridx = 7;
+		gbc_table_4.gridy = 0;
+		panel_1.add(table_4, gbc_table_4);
+		
+		table_8 = new JTable();
+		GridBagConstraints gbc_table_8 = new GridBagConstraints();
+		gbc_table_8.gridwidth = 3;
+		gbc_table_8.insets = new Insets(0, 0, 5, 0);
+		gbc_table_8.fill = GridBagConstraints.BOTH;
+		gbc_table_8.gridx = 8;
+		gbc_table_8.gridy = 0;
+		panel_1.add(table_8, gbc_table_8);
+		
+		table_6 = new JTable();
+		GridBagConstraints gbc_table_6 = new GridBagConstraints();
+		gbc_table_6.insets = new Insets(0, 0, 5, 5);
+		gbc_table_6.fill = GridBagConstraints.BOTH;
+		gbc_table_6.gridx = 0;
+		gbc_table_6.gridy = 1;
+		panel_1.add(table_6, gbc_table_6);
+		
+		lblNewLabel_4 = new JLabel("Total moms");
+		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4.gridx = 1;
+		gbc_lblNewLabel_4.gridy = 1;
+		panel_1.add(lblNewLabel_4, gbc_lblNewLabel_4);
+		
+		table_7 = new JTable();
+		GridBagConstraints gbc_table_7 = new GridBagConstraints();
+		gbc_table_7.insets = new Insets(0, 0, 5, 5);
+		gbc_table_7.fill = GridBagConstraints.BOTH;
+		gbc_table_7.gridx = 7;
+		gbc_table_7.gridy = 1;
+		panel_1.add(table_7, gbc_table_7);
+		
+		table_9 = new JTable();
+		GridBagConstraints gbc_table_9 = new GridBagConstraints();
+		gbc_table_9.gridwidth = 3;
+		gbc_table_9.insets = new Insets(0, 0, 5, 0);
+		gbc_table_9.fill = GridBagConstraints.BOTH;
+		gbc_table_9.gridx = 8;
+		gbc_table_9.gridy = 1;
+		panel_1.add(table_9, gbc_table_9);
+		
+		lblNewLabel_7 = new JLabel("Total pris");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_7.gridx = 1;
+		gbc_lblNewLabel_7.gridy = 2;
+		panel_1.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		table_5 = new JTable();
+		GridBagConstraints gbc_table_5 = new GridBagConstraints();
+		gbc_table_5.insets = new Insets(0, 0, 0, 5);
+		gbc_table_5.fill = GridBagConstraints.BOTH;
+		gbc_table_5.gridx = 7;
+		gbc_table_5.gridy = 2;
+		panel_1.add(table_5, gbc_table_5);
+		
+		table_10 = new JTable();
+		GridBagConstraints gbc_table_10 = new GridBagConstraints();
+		gbc_table_10.gridwidth = 3;
+		gbc_table_10.fill = GridBagConstraints.BOTH;
+		gbc_table_10.gridx = 8;
+		gbc_table_10.gridy = 2;
+		panel_1.add(table_10, gbc_table_10);
+		
+		btnNewButton = new JButton("Godkend");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridwidth = 3;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 4;
+		gbc_btnNewButton.gridy = 9;
+		getContentPane().add(btnNewButton, gbc_btnNewButton);
 	}
 
 }
